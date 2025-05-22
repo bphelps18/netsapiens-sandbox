6d662d297be2af57b35266f597216d4d2cdff8dc
bphelps18 <137330117+bphelps18@users.noreply.github.com>
2025-05-22 20:24:28 +0000

    Pending changes exported from your codespace

---
+++ custom.js
@@ -0,0 +1,4 @@
+var paybill = '<li><a href="https://grid4.com" target="_blank" class="header-link" > Pay my bill</a></li>';
+var opentix = '<li><a href="https://grid4.com" target="_blank" class="header-link" > Open a ticket</a></li>';
+$('.user-toolbar').prepend(paybill);
+$('.user-toolbar').prepend(opentix);
